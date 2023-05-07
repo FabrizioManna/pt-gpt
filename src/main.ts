18a6217b4c3f71f48153210fb6321c4911cd966c
mannafabrizio83 <mannafabrizio83@gmail.com>
2023-05-07 23:52:56 +0200

    Fix

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import {
-  ValidationPipe,
-  VersioningType,
-  VERSION_NEUTRAL,
-} from '@nestjs/common';
+import { ValidationPipe, VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Rest API PT-GPT')
-    .setDescription(
-      'Rest API for use chat-gpt for personal trainer of specific parameters',
-    )
+    .setTitle("Rest API PT-GPT")
+    .setDescription('Rest API for use chat-gpt for personal trainer of specific parameters')
     .setVersion('v' + process.env.API_VERSION)
     .build();
 
@@ -24,7 +19,7 @@ async function bootstrap() {
 
   app.enableVersioning({
     type: VersioningType.URI,
-    defaultVersion: [VERSION_NEUTRAL, 'v1', 'v2'],
+    defaultVersion: [VERSION_NEUTRAL, 'v1', 'v2']
   });
 
   await app.listen(3000);
